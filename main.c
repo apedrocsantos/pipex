242c6eab553012297b5459276ab0c1ccd752e5eb
anda-cun <anda-cun@student.42lisboa.com>
2023-06-19 16:54:29 +0100

    bah

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: anda-cun <anda-cun@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 15:59:10 by anda-cun          #+#    #+#             */
-/*   Updated: 2023/06/19 16:40:05 by anda-cun         ###   ########.fr       */
+/*   Updated: 2023/06/19 16:52:51 by anda-cun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,10 @@
 int main(int argc, char *argv[], char *envp[])
 {
     int i = 0;
-    char **commands[--argc/2];
+    char ***commands;
     
     argv++;
+    commands = (char ***)malloc(sizeof(char) * (argc/2 + 1));
     while (argc - i > 0)
     {
         if (argc % 2 == 0)
@@ -29,8 +30,9 @@ int main(int argc, char *argv[], char *envp[])
         i++;
     }
     i = 0;
-    // while (commands[i])
-        ft_printf("%s\n", *commands[0]);
+
+        while (commands[i])
+            ft_printf("%s\n", commands[i++]);
 }
 
 // /user/bin/cat    which cat
